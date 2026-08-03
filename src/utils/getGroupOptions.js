749d1bbeba31ef6c3f0b4d803461dfb27b179792
Morgan Mueller <84867869+mmueller1-NYCDOHMH@users.noreply.github.com>
2026-08-03 08:25:18 -0700

    Delete src directory

--- src/utils/getGroupOptions.js
+++
@@ -1,5 +0,0 @@
-export const getGroupOptions = (data = [], groupField = "submetric", allLabel = "All Groups") => {
-    const unique = [...new Set(data.map((d) => d[groupField]).filter(Boolean))];
-    return [allLabel, ...unique];
-  };
-  
